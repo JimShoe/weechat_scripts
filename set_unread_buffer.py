b3e3dfb6dcacbb9e5091f25313eba18ceb1e8f24
Nathan W <nathan@frcv.net>
2014-02-07 10:37:58 -0600

    Fist commit of my shorturl and set_unread_buffer scrips.

---
+++ set_unread_buffer.py
@@ -0,0 +1,15 @@
+import weechat
+
+def set_unread(data, buffer, args):
+  args = args.strip()
+  if args == "":
+      weechat.prnt("", "ERROR: NO ARGS")
+      return weechat.WEECHAT_RC_OK
+  buffer = weechat.buffer_search("",args)
+  if not buffer:
+      weechat.prnt("", "ERROR: couldn't find buffer: "+args)
+  weechat.command(buffer, "/input set_unread_current_buffer")
+  return weechat.WEECHAT_RC_OK
+
+weechat.register("set_unread_buffer", "JimShoe", "1.0", "GPL3", "Sets unread marker on buffer.", "", "")
+weechat.hook_command("set_unread", "Set unread marker on buffer", "", "", "", "set_unread", "")
---
+++ shorturl.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+import weechat
+import re
+
+SCRIPT_NAME    = "shorturl"
+SCRIPT_AUTHOR  = "JimShoe <nathan@frcv.net>"
+SCRIPT_VERSION = "0.1"
+SCRIPT_LICENSE = "GPL3"
+SCRIPT_DESC    = "Shortens url and puts them inline."
+
+def end_script():
+  return weechat.WEECHAT_RC_OK;
+
+# Calback from hook_process in shorten
+def print_short(data, command, rc, out, err):
+  # bail if there is an error
+  if rc != 0:
+    return weechat.WEECHAT_RC_OK
+  color = weechat.color(weechat.config_get_plugin("color"))
+  reset = weechat.color('reset')
+  shortener = weechat.config_get_plugin('shortener')
+  # parse response based on shortener setting
+  if shortener == 'rldn':
+    status, url = out.split()
+  if shortener == 'is.gd':
+    url = out
+  weechat.prnt(data, '%(color)s%(url)s%(reset)s' % dict(color=color,reset=reset,url=url))
+  return weechat.WEECHAT_RC_OK
+
+# call hook_process based on shortener setting
+def shorten(buffer, url):
+  shortener = weechat.config_get_plugin('shortener')
+  if shortener == 'rldn':
+    url = "url:http://rldn.net/api/%s" % (url,)
+  if shortener == 'is.gd':
+    url = "url:http://is.gd/create.php?format=simple&url=%s" % (url,)
+  weechat.hook_process(url,
+    30 * 1000, 
+    "print_short", 
+    buffer) 
+  return weechat.WEECHAT_RC_OK
+
+# lets you shorten multiple urls by typing /shorten URL...
+def shortenurl(data, buffer, args):
+  if len(args) == 0:
+    weechat.command("","/help shortenurl")
+  urls = re.findall('[a-z]{2,5}://[^\s()\[\]]*', args)
+  for url in urls:
+    shorten(buffer, url)
+  return weechat.WEECHAT_RC_OK
+
+# returns buffer using data from hook_modifier
+def getbuffer(modifier_data, string):
+  msg = weechat.info_get_hashtable("irc_message_parse",{ "message": string })
+  if weechat.info_get('irc_is_channel', '%s,%s' % (modifier_data, msg['channel'])) == '1':
+    name = msg['channel']
+  else:
+    name = msg['nick']
+  buffer_full_name = '%s.%s' % (modifier_data, name)
+  return weechat.buffer_search("irc", buffer_full_name)
+
+# Look for urls, color if too short, shorten if too long
+def modifier_cb(data, modifier, modifier_data, string):
+  urls = re.findall('[a-z]{2,5}://[^\s()\[\]]*', string)
+  for url in urls:
+    color = weechat.color(weechat.config_get_plugin("color"))
+    reset = weechat.color('reset')
+    urllength = int(weechat.config_get_plugin('urllength'))
+    if len(url) < urllength:
+      return re.sub(re.escape(url), "%(color)s%(url)s%(reset)s", string) % dict(color=color,reset=reset,url=url)
+    buffer = getbuffer(modifier_data, string)
+    shorten(buffer, url)
+  return string
+
+if __name__ == "__main__":
+  if weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, "end_script", ""):
+    weechat.hook_modifier("irc_in2_privmsg", "modifier_cb", "")
+    weechat.hook_command("shortenurl", "Shortens a url and prints it to your current buffer.", "[<URL...>]", 
+      "         URL: URL to shorten (multiple URLs may be given)\n", "", "shortenurl", "");
+
+    settings = {
+      'shortener': ('is.gd', 'URL Shortener to use.'),
+      'urllength': ('40', 'Max length of url before shortening.'),
+      'color': ('lightblue', 'Color for all urls.'),
+    }
+
+    version = weechat.info_get('version_number', '')
+    for option, default_desc in settings.iteritems():
+      if not weechat.config_is_set_plugin(option):
+        weechat.config_set_plugin(option, default_desc[0])
+      if int(version) >= 0x00030500:
+        weechat.config_set_desc_plugin(option, default_desc[1])
+
+
